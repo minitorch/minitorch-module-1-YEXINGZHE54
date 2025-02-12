944cdbe11b0c1b0f2f5b1f48e918e7189ebe4ea0
zhaoxing.li <zhaoxing.li@ushow.media>
2025-02-12 18:07:04 +0800

    todo: test scaar

--- minitorch/autodiff.py
+++ minitorch/autodiff.py
@@ -23,7 +23,10 @@ def central_difference(f: Any, *vals: Any, arg: int = 0, epsilon: float = 1e-6) 
         An approximation of $f'_i(x_0, \ldots, x_{n-1})$
     """
     # TODO: Implement for Task 1.1.
-    raise NotImplementedError("Need to implement for Task 1.1")
+    y1 = f(*vals)
+    vals[arg] += epsilon
+    y2 = f(*vals)
+    return (y2-y1) / epsilon
 
 
 variable_count = 1
